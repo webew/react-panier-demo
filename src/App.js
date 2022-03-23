d87da1ba8b44853769d944f4cd17364991a0db60
webew <fillion.bertrand@gmail.com>
2022-03-23 10:33:00 +0100

    produits panier + affichage panier

--- src/App.js
+++ src/App.js
@@ -13,23 +13,33 @@ class App extends Component {
   }
   async componentDidMount() {
     // Ici on initialise les données du composant
-    const response = await fetch("datas/datas.json");
+    const response = await fetch("https://localhost:8000/main");
+    // const response = await fetch("datas/datas.json");
     const datas = await response.json();
     datas.map(prod => prod.quantite = 0);
     console.log(datas);
     this.setState({ produits: datas });
   }
   produitChange(id, quantite) {
-    console.log("produit change dans App : ", id, quantite);
     // on va mettre à jour le catalogue
+    const produits = this.state.produits;
+    produits.map((produit) => { // on recherche dans le catalogue le produit en cours de modification
+      if (produit.id === id) {
+        produit.quantite = quantite;
+      }
+      return produit;
+    });
+    this.setState({ produits: produits }); // on met à jour le state pour refresh l'affichage de tous les composants
   }
   render() {
+    // on filtre dans le catalogue les produits dont la quantité est différente de 0 => produits du panier
+    const produitsPanier = this.state.produits.filter((produit) => { return produit.quantite !== 0 });
     return (
       <BrowserRouter>
         <Header />
         <Routes>
           <Route path='/catalogue' element={<Catalogue produits={this.state.produits} produitChange={(id, quantite) => this.produitChange(id, quantite)} />} />
-          <Route path='/panier' element={<Panier />} />
+          <Route path='/panier' element={<Panier produitsPanier={produitsPanier} />} />
           <Route path='/' element={<Accueil />} />
         </Routes>
       </BrowserRouter>
--- src/pages/Panier.jsx
+++ src/pages/Panier.jsx
@@ -6,8 +6,42 @@ class Panier extends Component {
         this.state = {}
     }
     render() {
+        let total = 0; // total du panier
         return (
-            <h1>Panier</h1>
+            <>
+                <h1>Panier</h1>
+                <table className='tablePanier'>
+                    <thead>
+                        <tr>
+                            <th>Nom</th>
+                            <th>Quantité</th>
+                            <th>Prix</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            this.props.produitsPanier.map((produit) => {
+                                total += (produit.quantite * produit.prix);
+                                return (
+                                    <tr key={produit.id}>
+                                        <td>{produit.nom}</td>
+                                        <td>{produit.quantite}</td>
+                                        <td>{produit.prix} €</td>
+                                        <td>{(produit.quantite * produit.prix).toFixed(2)} €</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colSpan={3}>Total</td>
+                            <td>{total.toFixed(2)}</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </>
         );
     }
 }
